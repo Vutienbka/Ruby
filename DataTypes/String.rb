a3da57f3a7e5f831640ea346d836bca27678222c
VuTienBnk <vutienbka@gmail.com>
2020-06-16 17:53:57 +0700

    Cap nhat moi

--- DataTypes/String.rb
+++ DataTypes/String.rb
@@ -22,3 +22,14 @@ p msg[0..5]
 
 #Noi chuoi
 p msg.concat(" programming","")
+# Kiem tra chuoi
+str1 = "ruby.com"
+str2 = "python.com"
+puts str1.start_with? "ruby"
+puts str2.end_with? ".com"
+puts str1.include? ".com"
+#In ra ca ky tu dieu khien
+# Phương thức inspect sẽ in ra ca các ký tự thoát ra
+msg = "Ruby\nPython"
+puts msg
+puts msg.inspect
