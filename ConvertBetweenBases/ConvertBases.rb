9d03094c07ca7082aa38c8b3ecc9bf5c0efd9f0f
vutien <vutienbka@gamil.com>
2020-06-16 22:09:22 +0700

    Update

---
+++ ConvertBetweenBases/ConvertBases.rb
@@ -0,0 +1,6 @@
+b = 20
+#Chuyen doi b sang co so 2
+c =  b.to_s(2)
+d = c.to_i
+puts d
+p d/2
---
+++ SwitchCase.rb
@@ -0,0 +1,9 @@
+
+
+a = 10
+case  a
+when 10
+puts "this is the number looking for"
+else
+  "this is not the number looking for"
+end
