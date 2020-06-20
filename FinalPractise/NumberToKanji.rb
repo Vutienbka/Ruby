e6b690de32c12a1b02fc402c39898cbc7fe0c6ce
vutien <vutienbka@gamil.com>
2020-06-20 23:12:46 +0700

    Finished

---
+++ FinalPractise/NumberToKanji.rb
@@ -0,0 +1,55 @@
+def to_Kansuji number
+  unit = ["〇", "一", "二", "三", "四", "五", "六", "七", "八", "九", "十", "十一", "十二", "十三", "十四", "十五", "十六", "十七", "十八", "十九"]
+  m_num = {:juu => "十", :hyaku => "百", :sen => "千", :man => "万", :ooku => "億", :chou => "兆", :kei => "京", :gai => "垓", :jo => "𥝱", :joo => "穣", :koo => "溝",
+            :kan => "澗", :sei => "正", :sai => "載", :goku => "極", :googasha => "恒河沙", :asoogi => "阿僧祇", :nayuta => "那由他", :fukashigi => "不可思議", :muryootaisuu => "無量大数"}
+  if number <= 0
+    return unit[0]
+    elsif number < 20
+    return unit[number]
+  elsif number < 100
+    return to_Kansuji(number / 10) + m_num[:juu] + to_Kansuji(number % 10)
+  elsif number < 200
+    return m_num[:hyaku]  + to_Kansuji(number % 100)
+  elsif number < 1000
+    return to_Kansuji(number / 100) + m_num[:hyaku] + to_Kansuji(number % 100)
+  elsif number < 2000
+    return m_num[:sen] + to_Kansuji(number % 1000)
+  elsif number < 10**4
+    return to_Kansuji(number / 1000) + m_num[:sen] + to_Kansuji(number % 1000)
+  elsif number < 10**8
+    return to_Kansuji(number / 10**4) + m_num[:man] + to_Kansuji(number % 10**4)
+  elsif number < 10**12
+    return to_Kansuji(number / 10**8) + m_num[:ooku] + to_Kansuji(number % 10**8)
+  elsif number < 10**16
+    return to_Kansuji(number / 10**12) + m_num[:chou] + to_Kansuji(number % 10**12)
+  elsif number < 10**20
+    return to_Kansuji(number / 10**16) + m_num[:kei] + to_Kansuji(number % 10**16)
+  elsif number < 10**24
+    return to_Kansuji(number / 10**20) + m_num[:gai] + to_Kansuji(number % 10**20)
+  elsif number < 10**28
+    return to_Kansuji(number / 10**24) + m_num[:jo] + to_Kansuji(number % 10**24)
+  elsif number < 10**32
+    return to_Kansuji(number / 10**28) + m_num[:joo] + to_Kansuji(number % 10**28)
+  elsif number < 10**36
+    return to_Kansuji(number / 10**32) + m_num[:koo] + to_Kansuji(number % 10**32)
+  elsif number < 10**40
+    return to_Kansuji(number / 10**36) + m_num[:kan] + to_Kansuji(number % 10**36)
+  elsif number < 10**44
+    return to_Kansuji(number / 10**40) + m_num[:sei] + to_Kansuji(number % 10**40)
+  elsif number < 10**48
+    return to_Kansuji(number / 10**44) + m_num[:sai] + to_Kansuji(number % 10**44)
+  elsif number < 10**52
+    return to_Kansuji(number / 10**48) + m_num[:goku] + to_Kansuji(number % 10**48)
+  elsif number < 10**56
+    return to_Kansuji(number / 10**52) + m_num[:googasha] + to_Kansuji(number % 10**52)
+  elsif number < 10**60
+    return to_Kansuji(number / 10**56) + m_num[:asoogi] + to_Kansuji(number % 10**56)
+  elsif number < 10**64
+    return to_Kansuji(number / 10**60) + m_num[:nayuta] + to_Kansuji(number % 10**60)
+  elsif number < 10**68
+    return to_Kansuji(number / 10**64) + m_num[:fukashigi] + to_Kansuji(number % 10**64)
+  else
+    return m_num[:muryootaisuu]
+  end
+end
+puts to_Kansuji 12345678
--- FinalPractise/Test3.rb
+++ FinalPractise/Test3.rb
@@ -1,8 +1,8 @@
 class StringToNumber
   def toString numberString
     result = 0
-    count = 0
-    $status = 0
+    flag = 0
+    $indexOfCurrentMnum = 0
     s_num = {"零" => 0, "一" => 1, "二" => 2, "三" => 3, "四" => 4, "五" => 5, "六" => 6,  "七" => 7, "八"=> 8, "九"=> 9 }
     m_num = { "十" => 10,"百" => 100, "千" => 1000, "万" => 10**4, "億" => 10**8, "兆" => 10**12, "京" => 10**16,
               "垓" => 10**20, "𥝱" => 10**24, "穣" => 10**28, "溝" => 10**32, "澗" => 10**36, "正" => 10**40, "載" => 10**44,
@@ -10,31 +10,35 @@ class StringToNumber
     allowedString = Hash[m_num.to_a.reverse]
     allowedString.each do |key, value|
       if numberString.include? key
-        # Lay index cua phan tu
-        $status = numberString.index(key)
-        tempArray = numberString[0,$status].chars.to_a
-        numberString = numberString[$status + 1, numberString.length - 1]
-        puts numberString
+        # Lay index cua phan tu m_num hien tai
+        $indexOfCurrentMnum = numberString.index(key)
+        # Cat chuoi dung truoc phan tu m_num luu vao mang ky tu tempArray
+        tempArray = numberString[0,$indexOfCurrentMnum].chars.to_a
+        # Cap nhat lai chuoi numberString sau khi cat chuoi phia truoc phan tu m_num
+        numberString = numberString[$indexOfCurrentMnum + 1, numberString.length - 1]
+        # chuoi tempArray se co hai truong hop xay ra hoac la Rong hoac khong Rong
         if tempArray.length > 0
+          # Dao nguoc chuoi de tinh toan gia tri cua chuoi
           tempArray = tempArray.reverse
           subResult = 0
-          check = true
+          check = false
           m_num.each do |ke,va|
             if tempArray.include? ke
-              check = false
+              check = true
               break
             end
           end
-          # neu check = true tuc la tempArray khong chua cac ky tu 百 chang han
-          if check
+          # Neu check = false tuc la tempArray khong chua cac ky tu m_num 百 chang han
+          # tuc la temArray chi chua cac k tu s_num
+          # Ta co the tinh toan nhu so 1230 = 1*10^3 + 2*10^2 + 3*10^1 + 0+10^0
+          if !check
             for index in 0..tempArray.length-1
               s_num.each do |k,v|
-                if k == tempArray[index]
-                  subResult += v * 10**index
-                end
+                subResult += v * 10**index if k == tempArray[index]
               end
             end
             result += value *  subResult
+            # Neu tempArray chua phan tu m_num thi ta se goi de quy ham "toString tempArray" cho den khi temArray chi chua cac phan tu s_num
           else
             tempArray = tempArray.join.reverse
             result += value * (toString tempArray)
@@ -44,26 +48,26 @@ class StringToNumber
             result += v if k == key
           end
         end
-        count +=1
       end
     end
+    # Ta se tinh toan chuoi sau phan tu m_num cuoi cung trong mang numberString
     if numberString.length > 0
       subString = numberString.chars.to_a
       for index in 0..subString.length-1
         s_num.each do |k,v|
-          if k == subString[index]
-            result += v * 10**index
-          end
+          result += v * 10**index if k == subString[index]
         end
       end
     end
     return result
   end
 end
-numberString = "九千九百九十九無量大数九千九百九十九不可思議九千九百九十九那由他九千九百九十九阿僧祇九千九百九十九恒河沙九千九百九十九極九千九百九十九載九千九百九十九正九千九百九十九澗九千九百九十九溝九千九百九十九穣九千九百九十九𥝱九千九百九十九垓九千九百九十九京九千九百九十九兆九千九百九十九億九千九百九十九万九千九百九十九"
+numberString = "四垓七千八百十二京八千四百七十九兆一千二百三十七億二千三百八十万三千四百五十八"
+# 478128479123723803458
+# numberString = "九千九百九十九無量大数九千九百九十九不可思議九千九百九十九那由他九千九百九十九阿僧祇九千九百九十九恒河沙九千九百九十九極九千九百九十九載九千九百九十九正九千九百九十九澗九千九百九十九溝九千九百九十九穣九千九百九十九𥝱九千九百九十九垓九千九百九十九京九千九百九十九兆九千九百九十九億九千九百九十九万九千九百九十九"
 stringToNumber = StringToNumber.new
 puts stringToNumber.toString(numberString)
-
-s1 = "999999999999999999999999999999999999999999999999999999999999999999999999"
-s2 = "999999999999999999999999999999999999999999999999999999999999999999999999"
+# 999999999999999999999999999999999999999999999999999999999999999999999999
+s1 = "478128479123723803458"
+s2 = "478128479123723803458"
 puts s1.eql? s2
